9091ee5aa90b4316a9f9ba7dc3c0173360d8918a
Brian Fitzgerald <brianjfitzgerald@gmail.com>
2023-10-17 19:34:46 -0400

    add Posts query

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-// import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -13,11 +14,13 @@ const main = async () => {
     await migrator.up();
 
     const app = express();
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false,
         }),
+        context: () => ({ em: orm.em }),
     });
 
     await apolloServer.start();
