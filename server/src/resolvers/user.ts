9fd042e85e486b80126584383a79f1a275fa57f6
Brian Fitzgerald <brianjfitzgerald@gmail.com>
2023-11-07 16:22:58 -0500

    formatting

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -65,6 +65,7 @@ export class UserResolver {
                 ],
             };
         }
+
         const hashedPassword = await argon2.hash(options.password);
         const user = em.create(User, {
             username: options.username,
